c2603742ebb69f039012b3c49aec1b3f7ce4e63c
Xenon506 <yuvrajsingh232415@gmail.com>
2025-11-17 12:52:38 +0530

    JS Begginer done

---
+++ Practice/pattern.js
@@ -0,0 +1,14 @@
+
+var a = 4;
+let arr = [];
+
+const boxPattern = (arr) => {
+    for(let i = 0; i<4 ; i++ ){
+        
+            console.log('*');
+    }
+}
+
+let b = boxPattern(arr);
+console.log(b);
+
